eb84f119bd4230b8b9c23df37fef324dd48fbfb4
John Qosimi <john.qosimi@cat.com>
2022-10-25 14:27:32 -0500

    Navigation details

--- mytwitchapp/src/App.js
+++ mytwitchapp/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne =()=>{
+  return <div>Page 1-One
+  <hr></hr>
+  <Link to="/pagetwo"> Navigate to Page Two </Link>
+  </div>
+
+}
+
+const PageTwo =()=>{
+  return <div>Page 2-Two
+  <hr></hr>
+  <button>Click Me</button>
+  <Link to="/"> Navigate to Page -1-One-Uno </Link></div>
+  
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <BrowserRouter>
+        <div className='container' >
+
+        <Route path ='/' exact component={PageOne} />
+        <Route path='/pagetwo' component={PageTwo} />
+        </div>
+
+    </BrowserRouter>
+      
     </div>
   );
 }
