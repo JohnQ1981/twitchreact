8374f1679928f31f1411da3e712158225ceb8789
John Qosimi <john.qosimi@cat.com>
2022-10-25 17:01:17 -0500

    added files

--- mytwitchapp/src/components/Header.js
+++ mytwitchapp/src/components/Header.js
@@ -7,9 +7,14 @@ const Header = () => {
       <Link to="/" className="item">
         Streamy
       </Link>
-      <div className="right manu">
+      <div className="right menu">
         <Link to="/" className="item">
           All Streams
+        </Link>
+      </div>
+      <div className="right menu">
+        <Link to="/" className="item">
+          Login with Google
         </Link>
       </div>
     </div>
