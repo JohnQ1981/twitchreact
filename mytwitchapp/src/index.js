0f6afaf8f9fdcfa3f344940ceafc688902ab0046
JohnQ1981 <ikram1981@gmail.com>
2022-10-25 21:44:14 -0500

    added some code for redux devtool extension

--- mytwitchapp/src/index.js
+++ mytwitchapp/src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers";
 
-const store =createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <Provider store={store} >
-    <App />
-    </Provider>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
   </React.StrictMode>
 );
 
