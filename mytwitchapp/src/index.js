a2c0572507c8f8180cc81004a4eaae051e818dc7
JohnQ1981 <ikram1981@gmail.com>
2022-10-26 14:37:05 -0500

    post request is working

--- mytwitchapp/src/index.js
+++ mytwitchapp/src/index.js
@@ -5,11 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-//import reduxThunk from 'redux-thunk;'
+import reduxThunk from 'redux-thunk';
 import reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
