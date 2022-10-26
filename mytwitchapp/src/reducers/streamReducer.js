ffd368e1a53e2c3c76f48f7e31a2185ef9c1c414
JohnQ1981 <ikram1981@gmail.com>
2022-10-26 18:28:07 -0500

    ad

---
+++ mytwitchapp/src/reducers/streamReducer.js
@@ -0,0 +1,11 @@
+import {
+FETCH_STREAM,FETCH_STREAMS,CREATE_STREAM,EDIT_STREAM,DELETE_STREAM
+}from './types';
+
+export default ( state =[], action) =>{
+    switch (action.type){
+        default:
+            return state;
+    }
+
+}
