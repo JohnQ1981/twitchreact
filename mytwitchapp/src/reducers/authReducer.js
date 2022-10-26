c3a71bcc8d82a9607ad22093102f664902f52126
JohnQ1981 <ikram1981@gmail.com>
2022-10-25 21:04:57 -0500

    fixed

--- mytwitchapp/src/reducers/authReducer.js
+++ mytwitchapp/src/reducers/authReducer.js
@@ -6,7 +6,7 @@ const authReducer = (state = { INITIAL_STATE }, action) => {
   switch (action.type) {
     case "SIGN_IN":
       return { ...state, isSignedIn: true };
-    case "SIGN-OUT":
+    case "SIGN_OUT":
       return { ...state, isSignedIn: false };
     default:
       return state;
