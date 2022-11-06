03f072e471e9239046ccc14482000732111af553
JohnQ1981 <ikram1981@gmail.com>
2022-11-06 00:37:16 -0500

    added some buttons

--- mytwitchapp/src/actions/index.js
+++ mytwitchapp/src/actions/index.js
@@ -1,5 +1,6 @@
 import { SIGN_OUT, SIGN_IN, CREATE_STREAM, FETCH_STREAM,FETCH_STREAMS,DELETE_STREAM,EDIT_STREAM } from "../reducers/types";
 import streams from "../apis/streams";
+//import { get } from "lodash";
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
@@ -13,10 +14,13 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response =await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({type: CREATE_STREAM , payload: response.data});
+  //do some programmatic navigation to get the user back to the roo route
+  
 };
 
   export const fetchStreams = () => async dispatch => {
